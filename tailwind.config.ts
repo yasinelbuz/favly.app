b24c12eac750a388b9ff838338881f824ad17d84
Yasin Elbüz <yasinelbuz@gmail.com>
2024-12-14 02:34:20 +0300

    Saying bismillah, I am making my first commit to the project. 🚀

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,14 +5,8 @@ export default {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./style/**/*.{css}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
+  theme: {},
   plugins: [],
 } satisfies Config;
